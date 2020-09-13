da38a2e49bbf690414f438c1c22eed1eb2ebbe6f
Kieran Ryan <kryan@ub.edu.bz>
2020-09-13 10:38:41 -0600

    trying to fix and gate images

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ var inputTwoIsOn = false;
 
 
 function toggleImage() {
-   var img1 = "https://www.iconsdb.com/icons/preview/red/button-off-xxl.png";
-   var img2 = "https://www.iconsdb.com/icons/preview/green/button-on-xxl.png";
+   var img1 = "button-off-xxl.png";
+   var img2 = "button-on-xxl.png";
   
    
    var imgElement = document.getElementById('toggleImage');
@@ -17,8 +17,8 @@ function toggleImage() {
 
   
 function toggleImage2() {
-   var img1 = "https://www.iconsdb.com/icons/preview/red/button-off-xxl.png";
-   var img2 = "https://www.iconsdb.com/icons/preview/green/button-on-xxl.png";
+   var img1 = "button-off-xxl.png";
+   var img2 = "button-on-xxl.png";
   
    
    var imgElement = document.getElementById('toggleImage2');
